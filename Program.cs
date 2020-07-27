948d095ab6d1b8a84a60cd8f75cde2ef7d236230
Lilit.Hakobyan <lilit.hakobyan@helpsystems.com>
2020-07-27 17:44:37 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML;
+using System;
+
+namespace MLNET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MLContext mlContext = new MLContext();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
